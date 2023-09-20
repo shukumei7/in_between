90eed70acc5e63fb47b1451ee227eb7cd2805963
133137 <u90116286@infong-us1.perfora.net>
2023-09-19 23:40:24 -0400

    IONOS settings

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'url' => 'db5014524972.hosting-data.io', // env('DATABASE_URL'),
+            'host' => 'db5014524972.hosting-data.io', // env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'dbs12072939', // env('DB_DATABASE', 'forge'),
+            'username' => 'dbu5500394', // env('DB_USERNAME', 'forge'),
+            'password' => 'Z$6JNy5R@.Tgg*3A3!v35P', // env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
